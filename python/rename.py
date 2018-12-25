0dec17db0c4f024d376ad829ec3efab6ff51f95e
Qiao Yu <691761071@qq.com>
2018-12-25 15:11:55 +0800

    move three python files

---
+++ python/rename.py
@@ -0,0 +1,18 @@
+#coding:utf8
+import os;
+
+def rename():
+        i=0
+        path=r"C:\Users\69176\Desktop\pictures";
+        filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
+        for files in filelist:#遍历所有文件
+            i=i+1
+            Olddir=os.path.join(path,files);#原来的文件路径                
+            if os.path.isdir(Olddir):#如果是文件夹则跳过
+                    continue;
+            filename=os.path.splitext(files)[0];#文件名
+            filetype=os.path.splitext(files)[1];#文件扩展名
+            newName = "news"+ str(i)
+            Newdir=os.path.join(path,newName+filetype);#新的文件路径
+            os.rename(Olddir,Newdir)#重命名
+rename()
